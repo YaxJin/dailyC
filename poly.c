d02c05224e4aa59bc6b030ea6a8748fe1c19a994
Ya Jin <qooq147852@gmail.com>
2022-03-19 13:55:28 +0800

    Update poly.c

--- poly.c
+++ poly.c
@@ -3,9 +3,9 @@
 #include<math.h>
 
 struct Node {  
-  int cof;  
-  int exp;      
-  struct Node * next;  
+    int cof;  
+    int exp;      
+    struct Node * next;  
 }* poly = NULL;
 
 int inputNum(char *msg){
@@ -22,49 +22,48 @@ int inputNum(char *msg){
 }
 
 void create() {
-  struct Node * t, * last = NULL;  //temporary pointer, last pointer
-  int num, i;
-//   printf("Enter the number of terms:");
-  num = inputNum("Enter the number of terms:");
-    //   scanf("%d", & num);
-  fprintf(stderr, "Enter each term with cof and exp:\n");
-  for (i = 0; i < num; i++) {
-    t = (struct Node * ) malloc(sizeof(struct Node));  //create new node
-    scanf("%d%d", &t->cof, &t->exp);                    //reading  2 data 
-    t-> next = NULL;              //linking each node into linklist
-    if (poly == NULL) {
-      poly = last = t;
-    } else {
-      last -> next = t;
-      last = t;
+    struct Node * t, * last = NULL;  //temporary pointer, last pointer
+    int num, i;
+    num = inputNum("Enter the number of terms:");
+    fprintf(stderr, "Enter each term with cof and exp:\n");
+    for (i = 0; i < num; i++) {
+        t = (struct Node * ) malloc(sizeof(struct Node));   //create new node
+        scanf("%d%d", &t->cof, &t->exp);                    //reading  2 data 
+        t-> next = NULL;    //linking each node into linklist
+        if (poly == NULL)
+            poly = last = t;
+        else {
+            last -> next = t;
+            last = t;
+        }
     }
-  }
 }
 
 void Display(struct Node * p) {
-  while (p) {
-    if(!(p -> next))
-        printf("%dx%d =", p -> cof, p -> exp);   //printing last node
-    else
-        printf("%dx%d + ", p -> cof, p -> exp);   //printing node
-    p = p -> next;
-  }
+    while (p) {
+        if(!(p -> next))
+            printf("%dx^%d =", p -> cof, p -> exp);   //printing last node
+        else
+            printf("%dx^%d + ", p -> cof, p -> exp);   //printing node
+        p = p -> next;
+    }
   printf("\n");
 }
 
 long Eval(struct Node * p, int x) {       //evalution
-  long val = 0;
-  while (p) {
-    val += p -> cof * pow(x, p -> exp);
-    p = p -> next;
-  }
-  return val;
+    long val = 0;
+    while (p) {
+        val += p -> cof * pow(x, p -> exp);
+        p = p -> next;
+    }
+    return val;
 }
 
 int main() {
-  create();
-  Display(poly);
-  //while(fget!=NULL);//TODO endless change X valus 
-  printf("%ld\n", Eval(poly, 0));
-  return 0;
+    create();
+    Display(poly);
+    int x;
+    while((x=inputNum("please give x a value:")))
+        printf("%ld\n", Eval(poly, x));
+    return 0;
 }
