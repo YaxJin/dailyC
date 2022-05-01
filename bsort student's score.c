7db87b4e7dc1477eb68165521fe81d12928d30b0
Ya Jin <qooq147852@gmail.com>
2022-05-01 17:55:35 +0800

    Update bsort student's score.c

--- bsort student's score.c
+++ bsort student's score.c
@@ -8,38 +8,29 @@ struct student
 
 void input(struct student list[], int N)
 {
-    int i;
-    for (i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        printf("Enter data for Student #%d\n", i + 1);
-        
-        printf("Enter ID & score:");
+        // printf("Enter data for Student #%d\n", i + 1);
+        fprintf(stderr, "Enter ID & score:");
         scanf("%d %d", &list[i].id,  &list[i].score);
-
-        // printf("Enter score :");
-        // scanf("%d", &list[i].score);
     } 
 }
 
 void display(struct student list[], int N)
 {
-    int i;
-    
     printf("\nID\tscore\n");
-    for (i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         printf("%d\t%d\n", list[i].id, list[i].score);
     } 
 }
 
-void busort(struct student list[], int N)
+void busort(struct student list[], int N) //bubble sort
 {
-    int i, j;
-    struct student temp;
-    
-    for (i = 0; i < N - 1; i++)
+    struct student temp;   
+    for (int i = 0; i < N - 1; i++)
     {
-        for (j = 0; j < (N - 1-i); j++)
+        for (int j = 0; j < (N - 1-i); j++)
         {
             if (list[j].id > list[j + 1].id)
             {
@@ -56,15 +47,16 @@ int main()
     struct student data[40]; //student upperbound
     int n;
 
-    printf("Number of records you want to keyin:");
+    fprintf(stderr, "Number of records you want to key in:");
     scanf("%d", &n);
 
     input(data, n);
     // printf("\nBefore sorting");
     // display(data, n);
     busort(data, n);
-    printf("\nAfter sorting");
+    // printf("\nAfter sorting");
     display(data, n);
 
     return 0;
-} 
+}
+
